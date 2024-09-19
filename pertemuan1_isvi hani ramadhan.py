321a0e327f641a8a960a7f64b58d6c8e2b4699b0
haniboy87 <154058137+haniboy87@users.noreply.github.com>
2024-09-19 20:02:43 +0700

    Add files via upload

---
+++ pertemuan1_isvi hani ramadhan.py
@@ -0,0 +1,11 @@
+nama = "isvi hani ramadhan"
+nama_panggilan = "vihan"
+
+print("Nama saya,", nama)
+print("Kamu bisa panggil saya", nama_panggilan)
+
+nama = input("masukkan nama ")
+nama_panggilan = input("masukkan nama panggilan kamu: ")
+
+print("Nama saya, " + nama)
+print("Kamu bisa panggil saya", nama_panggilan)
